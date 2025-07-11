53112f7529924347b16842506d839dee719503f5
Werner Brown <51110410+WernerBrownJr@users.noreply.github.com>
2025-07-11 11:08:35 +0200

    Updated + animation added

--- ThreeJS/main.js
+++ ThreeJS/main.js
@@ -4,72 +4,96 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { AnimationMixer } from 'three';
+
+const canvas = document.getElementById("experience-canvas");
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const scene = new THREE.Scene();
-const canvas = document.getElementById("experience-canvas");
+const loader = new GLTFLoader();
+
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+const controls = new OrbitControls(camera, canvas);
+
+const sun = new THREE.DirectionalLight(0xffffff, 1.5);
+const light = new THREE.DirectionalLight(0xFFFFFF);
+const clock = new THREE.Clock();
 
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.VSMShadowMap;
+let mixer;
 
-// Loader //
-const loader = new GLTFLoader();
-loader.load('Scene_revised04.glb', function (glb) {
-    glb.scene.traverse(child => {
-        if (child.isMesh) {
-            child.castShadow = true;
-            child.receiveShadow = true;
-            console.log(child);
-        }
-    })
-    scene.add(glb.scene);
-
-}, undefined, function (error) {
-
-    console.error(error);
-
-});
-// // // // 
+// SCENE //
+scene.background = new THREE.Color(0x111111);
+// // // //
 
 // Global light //
+light.position.set(-50, -50, -12);
+light.intensity = 0.1;
+light.castShadow = false;
 
-// Add something to make scene brighter
-
+scene.add(light);
+// This makes the shadows and dark spots of the buildings look a little less dark
+// Making it better visually and more pleasing to the eye
 // // // // // // 
 
 // Sunlight //
-
-const sun = new THREE.DirectionalLight(0xffffff, 1.5);
 sun.castShadow = true;
-sun.position.set(50, 200, 30);
+sun.position.set(15, 30, 5);
 sun.shadow.camera.left = -20;
 sun.shadow.camera.right = 20;
 sun.shadow.camera.top = 20;
 sun.shadow.camera.bottom = -20;
 sun.shadow.normalBias = 0.2;
-scene.add(sun);
+sun.shadow.intensity = 0.5;
+sun.shadow.mapSize.height = 4096;
+sun.shadow.mapSize.width = 4096;
 
+scene.add(sun);
 // // // // //
 
 // CAMERA  // 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 camera.position.z = 30;
 camera.position.x = 30;
 camera.position.y = 20;
 
 // CAMERA CONTROLS //
-const controls = new OrbitControls(camera, canvas);
 controls.update();
+// // // // // // //
 
-// // // // // // // 
+// RENDERER //
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.VSMShadowMap;
+// // // // // 
+
+
+// LOADER //
+loader.load('Scene8.glb', function (glb) {
+    const model = glb.scene;
+    const animations = glb.animations;
+
+    model.traverse(child => {
+        if (child.isMesh) {
+            child.castShadow = true;
+            child.receiveShadow = true;
+        }
+    })
+    scene.add(glb.scene);
+
+    mixer = new THREE.AnimationMixer(model);
+    animations.forEach((clip) => {
+        mixer.clipAction(clip).play();
+    });
+
+}, undefined, function (error) {
+    console.error(error);
+});
+// // // // 
 
 function handleResize() {
     sizes.width = window.innerWidth;
@@ -85,10 +109,9 @@ window.addEventListener("resize", handleResize);
 function animate() {
     renderer.render(scene, camera);
 
-    // vehicle animation
+    const delta = clock.getDelta();
+    if (mixer) mixer.update(delta);
+
 }
 renderer.setAnimationLoop(animate);
 
-function copChaseAnimate() {
-
-}
